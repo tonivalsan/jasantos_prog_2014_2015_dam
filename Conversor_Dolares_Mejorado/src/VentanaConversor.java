5b1a53980a7fd34352dd4e3ee3fd519967c828b6
jasantos <jasantosdam@gmail.com>
2014-12-14 23:36:10 +0100

    Conversor mejorado con monedas

--- Conversor_Dolares_Mejorado/src/VentanaConversor.java
+++ Conversor_Dolares_Mejorado/src/VentanaConversor.java
@@ -5,12 +5,20 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
+
 import java.awt.Font;
+
 import javax.swing.JTextField;
 import javax.swing.JButton;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Color;
+
+import javax.swing.JComboBox;
+
+import java.awt.event.ItemListener;
+import java.awt.event.ItemEvent;
 
 
 public class VentanaConversor extends JFrame {
@@ -19,7 +27,7 @@ public class VentanaConversor extends JFrame {
 	private JTextField cajaEuros;
 	private JTextField cajaDolares;
 	private JTextField cajaMensaje;
-
+	JComboBox comboBox = new JComboBox();
 	/**
 	 * Launch the application.
 	 */
@@ -51,26 +59,61 @@ public class VentanaConversor extends JFrame {
 		//Texto Euros
 		JLabel lblEuros = new JLabel("Euros");
 		lblEuros.setFont(new Font("Tahoma", Font.BOLD, 11));
-		lblEuros.setBounds(21, 67, 93, 14);
+		lblEuros.setBounds(21, 69, 93, 14);
 		contentPane.add(lblEuros);
 		
 		//En el siguiente cuadro se escriben los euros
 		cajaEuros = new JTextField();
-		cajaEuros.setBounds(21, 87, 150, 20);
+		cajaEuros.setBounds(21, 89, 150, 20);
 		contentPane.add(cajaEuros);
 		cajaEuros.setColumns(10);
-		
-		//Texto Dólares
-		JLabel lblDlares = new JLabel("D\u00F3lares");
-		lblDlares.setFont(new Font("Tahoma", Font.BOLD, 11));
-		lblDlares.setBounds(21, 134, 93, 14);
-		contentPane.add(lblDlares);
 		
 		//En el siguiente cuadro se muestra la conversión de euros a dólares
 		cajaDolares = new JTextField();
 		cajaDolares.setColumns(10);
-		cajaDolares.setBounds(21, 155, 150, 20);
+		cajaDolares.setBounds(21, 164, 150, 20);
 		contentPane.add(cajaDolares);
+		
+		
+		comboBox.setBounds(21, 133, 150, 20);
+		contentPane.add(comboBox);
+		
+		String monedas[]=new String[4];
+		monedas[0]="Dólares";
+		monedas[1]="Libras";
+		monedas[2]="Euros";
+		monedas[3]="Yen";
+		
+		comboBox.addItem(monedas[0]);
+		comboBox.addItem(monedas[1]);
+		comboBox.addItem(monedas[2]);
+		comboBox.addItem(monedas[3]);
+		
+
+		comboBox.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent arg0) {
+				switch((String)comboBox.getSelectedItem()){
+				
+				case "Dólares":
+					cajaMensaje.setText("Moneda elegida: Dólares");
+					break;
+					
+				case "Libras":
+					cajaMensaje.setText("Moneda elegida: Libras");
+					break;
+					
+				case "Euros":
+					cajaMensaje.setText("Moneda elegida: Euros");
+					break;
+					
+				default:
+					cajaMensaje.setText("Moneda elegida: Yen");
+					break;
+				
+				}
+			}
+		});
+		
 		
 		//Botón convertir
 		JButton btnConvertir = new JButton("Convertir");
@@ -83,7 +126,10 @@ public class VentanaConversor extends JFrame {
 				//Intentamos los que se muestra a continuación
 				try{
 					euros=Float.parseFloat(cajaEuros.getText());					
-					cajaDolares.setText(String.valueOf(euros*valorConversion));
+					cajaDolares.setText(String.valueOf(euros*3));
+					
+					
+					
 					if(euros>0 && euros<=500){
 						cajaMensaje.setText("TODO OK");	
 					}else if(euros<=0){
@@ -100,7 +146,7 @@ public class VentanaConversor extends JFrame {
 			}
 		});
 		
-		btnConvertir.setBounds(208, 154, 89, 23);
+		btnConvertir.setBounds(208, 163, 89, 23);
 		contentPane.add(btnConvertir);
 		
 		//Texto Mensaje de control
@@ -117,5 +163,10 @@ public class VentanaConversor extends JFrame {
 		cajaMensaje.setColumns(10);
 		cajaMensaje.setBounds(21, 228, 276, 20);
 		contentPane.add(cajaMensaje);
+		
+		
+		
+		
+		
 	}
 }
--- Desplegable/src/Desplegable.java
+++ Desplegable/src/Desplegable.java
@@ -5,11 +5,17 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JComboBox;
+import java.awt.event.ItemListener;
+import java.awt.event.ItemEvent;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
 
 
 public class Desplegable extends JFrame {
 
 	private JPanel contentPane;
+	JComboBox comboBox = new JComboBox();
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -42,18 +48,58 @@ public class Desplegable extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JComboBox comboBox = new JComboBox();
+
 		comboBox.setBounds(32, 30, 153, 20);
 		contentPane.add(comboBox);
 		
-		comboBox.addItem("Jose Antonio");
-		String nombre="Manoli";
-		comboBox.addItem(nombre);
-		comboBox.addItem("Sofia");
-		comboBox.addItem("Mario");
+		String nombres[]=new String[4];
+		nombres[0]="Jose Antonio";
+		nombres[1]="Manoli";
+		nombres[2]="Sofia";
+		nombres[3]="Mario";
 		
-		comboBox.setSelectedItem("Sofia");
+		comboBox.addItem(nombres[0]);
+		comboBox.addItem(nombres[1]);
+		comboBox.addItem(nombres[2]);
+		comboBox.addItem(nombres[3]);
 		
+		comboBox.setSelectedItem(nombres[2]);
+		
+			
+		comboBox.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent arg0) {
+				switch((String)comboBox.getSelectedItem()){
+				
+				case "Jose Antonio":
+					textField.setText("Jose Antonio es el padre");
+					break;
+					
+				case "Manoli":
+					textField.setText("Manoli es la madre");
+					break;
+					
+				case "Sofia":
+					textField.setText("Sofia es la hija mayor");
+					break;
+					
+				default:
+					textField.setText("Sólo queda Mario, el pequeño de la casa");
+					break;
+				
+				}
+				
+				
+			}
+		});
+		
+		textField = new JTextField();
+		textField.setBounds(32, 131, 402, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblEleccin = new JLabel("Elecci\u00F3n");
+		lblEleccin.setBounds(32, 106, 277, 14);
+		contentPane.add(lblEleccin);
 		
 	}
 }
